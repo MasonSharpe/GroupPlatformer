ce9373a8c1d51e72b651cf9f460f22b3c1356cc3
MasonSharpe <115174711+MasonSharpe@users.noreply.github.com>
2022-12-01 09:40:09 -0800

    ASDSADSASD

--- RamioPlatformer/Assets/Scripts/Movement.cs
+++ RamioPlatformer/Assets/Scripts/Movement.cs
@@ -173,7 +173,7 @@ public class Movement : MonoBehaviour
         {
             grounded = true;
         }
-        else if (collision.gameObject.layer == 9)
+        else if (collision.gameObject.tag == "Spaceship" && parts >= partsNeeded)
         {
             SceneManager.LoadScene("level " + (level + 1));
         }
@@ -214,13 +214,6 @@ public class Movement : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Spaceship" && parts >= partsNeeded)
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-    }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
